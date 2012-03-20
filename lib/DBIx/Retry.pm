5ed2d511834f09ed12d0cb6f2ea8ed0f6c5353f8
Hartmut Behrens <hartmut.behrens@gmail.com>
2012-03-20 11:13:08 +0200

    yet more POD changes

--- lib/DBIx/Retry.pm
+++ lib/DBIx/Retry.pm
@@ -42,16 +42,16 @@ sub BUILDARGS {
 
 =head1 SYNOPSIS
 	
-	use DBIx::Retry;
-	my $conn = DBIx::Retry->new($dsn, $user, $tools, {retry_time => 5});
+  use DBIx::Retry;
+  my $conn = DBIx::Retry->new($dsn, $user, $tools, {retry_time => 5});
 	
-	# all other method are inherited from DBIx::Connector
-	my $dbh = $conn->dbh;  #get a database handle
+  # all other method are inherited from DBIx::Connector
+  my $dbh = $conn->dbh;  #get a database handle
 	
-	# Do something with the handle - will retry for specified amount of time, should the database not be available
-	$conn->run(fixup => sub {
-		$_->do('INSERT INTO foo (name) VALUES (?)', undef, 'Fred' );
-	});
+  # Do something with the handle - will retry for specified amount of time, should the database not be available
+  $conn->run(fixup => sub {
+    $_->do('INSERT INTO foo (name) VALUES (?)', undef, 'Fred' );
+  });
 	
 
 =head1 DESCRIPTION
@@ -62,13 +62,13 @@ DBIx::Retry is extended from DBIx::Connector. It adds the ability to keep retryi
 	
 Simply create a new DBIx::Retry object:
 	
-	my $conn = DBIx::Retry->new($dsn, $user, $tools, {retry_time => 5});
+  my $conn = DBIx::Retry->new($dsn, $user, $tools, {retry_time => 5});
 		 
 Then wrap your operations inside the run method that is inherited from DBIx::Connector:
 	
-	$conn->run(fixup => sub {
-		$_->do('INSERT INTO foo (name) VALUES (?)', undef, 'Fred' );
-	});
+  $conn->run(fixup => sub {
+    $_->do('INSERT INTO foo (name) VALUES (?)', undef, 'Fred' );
+  });
 	
 Should a database timout occur, DBIx::Retry will retry to connect to the database for the amount of	seconds specified in the "retry_time" attribute.
 
@@ -82,7 +82,6 @@ Should a database timout occur, DBIx::Retry will retry to connect to the databas
 =method new
 
 Create a new DBIx::Retry object.
-
 	my $conn = DBIx::Retry->new($dsn, $user, $tools,{timeout => 5, verbose => 1});
 	
 	
